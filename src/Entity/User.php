f2abbe85c935a62acd3fadde1bf2d5ada8a2d81c
zlatkoverk <zlatko112@gmail.com>
2019-11-07 20:04:22 +0100

    postgres driver

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -8,6 +8,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="useraccount")
  * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface
