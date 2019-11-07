3c0206cfa1e5e091d0c2f58db2ce6a265a894b21
zlatkoverk <zlatko112@gmail.com>
2019-11-07 22:51:04 +0100

    Add new item

--- src/Controller/TodoController.php
+++ src/Controller/TodoController.php
@@ -7,13 +7,14 @@ namespace App\Controller;
 use App\Entity\TodoItem;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class TodoController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", methods={"GET"})
      * @Security("has_role('ROLE_USER')")
      */
     public function index()
@@ -21,5 +22,27 @@ class TodoController extends AbstractController
         $todos = $this->getDoctrine()->getRepository(TodoItem::class)->findAll();
 
         return $this->render('todo/index.html.twig', ['todos' => $todos]);
+    }
+
+    /**
+     * @Route("/", methods={"POST"})
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function addTodoAction(Request $request)
+    {
+        $todo = new TodoItem();
+        $todo->setName($request->get('name', ''));
+        $todo->setLabel($request->get('label', ''));
+        $todo->setUser($this->getUser());
+
+        $dueDate = $request->get('dueDate');
+        if (!empty($dueDate)) {
+            $todo->setDueDate($dueDate);
+        }
+
+        $this->getDoctrine()->getManager()->persist($todo);
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->redirect('/');
     }
 }
